65af4b5cf73648222ed506315ce5ceefcd11d1a2
supersixaxis <appert.nathan@outlook.fr>
2024-02-14 11:57:06 +0100

    suppr folder useless

--- src/component/bookList.jsx
+++ src/component/bookList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { deleteBookIDB } from '../utils/bookServices'
 const defaultImageUrl = 'https://plus.unsplash.com/premium_photo-1681825268400-c561bd47d586?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
